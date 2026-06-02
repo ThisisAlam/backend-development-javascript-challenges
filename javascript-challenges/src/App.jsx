39381abb84284cad594c83e478e6b561d89c9c78
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-06-02 21:44:46 +0500

    added assets to the library

--- javascript-challenges/src/App.jsx
+++ javascript-challenges/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import StockTicker from './components/StockTicker.jsx';
-import SearchContact from './components/SearchContact.jsx'; 
+import SearchContact from './components/SearchContact.jsx';
+import RealEstate from './components/RealEstate.jsx'; 
 
 export default function App() {
 
@@ -10,13 +11,17 @@ export default function App() {
     <div className="App">
       <h1>JavaScript Challenges</h1>
       <br /><hr /><br />
-
-      <h2>Challenge no. 1: Stock Ticker</h2>
-      <StockTicker />
+      
+      <h2>Challenge no. 3: Real Estate</h2>
+      <RealEstate />
       <br />
 
       <h2>Challenge no. 2: Search Contact</h2>
       <SearchContact />
+      <br />
+
+      <h2>Challenge no. 1: Stock Ticker</h2>
+      <StockTicker />
       <br />
     </div>
   );
